e40461e48fc977d9358ac7eaf8d11b71125e8f95
jsngomes@yahoo.com <jsngomes@yahoo.com>
2018-03-29 00:11:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function placeOrder(cardNumber) {
     for(var i = 0; i < cart.length; i++) {
       if(cardNumber > 0){
       return  `Your total cost is $${cart[0].itemPrice}, which will be charged to the card ${cardNumber}.`
-    } else(){
+    } else(''){
       return 'Your total cost is $71, which will be charged to the card 83296759.'
     }
   
